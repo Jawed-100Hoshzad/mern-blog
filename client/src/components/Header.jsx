d5bba0a483ba5f5067717d7aaab8077a8444044a
Jawed-100Hoshzad <jdhoshzad100@gmail.com>
2024-01-23 10:47:53 +0430

    create and running of server side

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,12 +27,12 @@ export default function Header(){
             </Button>
 
             <div className="flex gap-2  md:order-2">
-                <Button className="w-12 h-10 lg:hidden sm:inline" color="gray">
+                <Button className="w-12 h-10  sm:inline" color="gray">
                     <FaMoon />
                 </Button>
 
                 <Link to="/signin">
-                    <Button gradientDuoTone='purpleToBlue'>
+                    <Button gradientDuoTone='purpleToBlue' outline>
                         SignIn
                     </Button>
                 </Link>
